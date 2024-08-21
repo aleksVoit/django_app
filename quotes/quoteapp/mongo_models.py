89a51fbd569d7e3408346f347fd3ba638a818e50
Aleksandr <aleksandr.voitushenko@gmail.com>
2024-08-21 18:22:57 +0200

    added script for loading mongo_db data

---
+++ quotes/quoteapp/mongo_models.py
@@ -0,0 +1,15 @@
+from mongoengine import Document
+from mongoengine.fields import ListField, StringField, ReferenceField
+
+
+class Author(Document):
+    fullname = StringField()
+    born_date = StringField()
+    born_location = StringField()
+    description = StringField()
+
+
+class Quote(Document):
+    tags = ListField()
+    author = StringField()
+    quote = StringField()
